41ce39f68a91ad46bb152a99356178049b010b3e
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-03-02 07:20:08 +0900

    react hooks使ってみる

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -5,64 +5,47 @@ import * as FooBar from './components/FooBar';
 import Hoge from './components/Hoge'
 
 
-// Reactコンポーネントを継承して、Blogクラスを作成
-class Blog extends React.Component {
-  // クラスなので、コンストラクタの初期化が必要
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPublished: false,
-      // count state設定
-      count: 0
-    }
-  }
+const Blog = () => {
 
-  componentDidMount(){
-    // ボタンがクリックされたら「いいね」をカウントアップする
-    document.getElementById("counter").addEventListener('click', this.countUp)
-  }
+  // componentDidMount(){
+  //   // ボタンがクリックされたら「いいね」をカウントアップする
+  //   document.getElementById("counter").addEventListener('click', this.countUp)
+  // }
 
-  componentDidUpdate(){
-    console.log(this.state.count)
-    // クリックするごとに呼ばれているが、条件が当てはまる時だけ、処理が走る
-    if (this.state.count >= 10) {
-      this.setState({count: 0})
-    }
-  }
+  // componentDidUpdate(){
+  //   console.log(this.state.count)
+  //   // クリックするごとに呼ばれているが、条件が当てはまる時だけ、処理が走る
+  //   if (this.state.count >= 10) {
+  //     this.setState({count: 0})
+  //   }
+  // }
 
-  componentDidWillMount(){
-    document.getElementById("counter").removeEventListener('click', this.countUp)
-  }
-  // 公開状態を反転させる関数
-  togglePublished = () => {
-    this.setState({
-      isPublished: !this.state.isPublished
-    })
-  };
+  // componentDidWillMount(){
+  //   document.getElementById("counter").removeEventListener('click', this.countUp)
+  // }
 
-  countUp = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
 
-  render() {
-    return(
-      <div>
-        <Article
-          title={"React"}
-          isPublished={this.state.isPublished}
-          toggle={() => this.togglePublished()}
-          count={this.state.count}
-        />
-        {/* <Foo /> */}
-        {/* <Bar /> */}
-        <FooBar.Foo />
-        <FooBar.Bar />
-        <Hoge />
-      </div>
-    )
-  }
+
+  // countUp = () => {
+  //   this.setState({
+  //     count: this.state.count + 1
+  //   })
+  // }
+
+  // render() {
+  return(
+    <>
+      <Article
+        title={"React"}
+      />
+      {/* <Foo /> */}
+      {/* <Bar /> */}
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </>
+  )
+  // }
 }
 
 export default Blog
